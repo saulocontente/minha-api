d4930137999363e3e70281175395f3f236ac1dd4
Saulo Contente <scontente@aliancahospitalar.com>
2020-06-12 21:46:19 -0300

    Implement methods to GET

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const data = require('./data.json')
 const resourceEndpoint = '/client'
 const param = '/:id'
 
+server.use(express.json())
+
+server.get(resourceEndpoint, function(request, response){
+    response.json(data)
+})
+
+server.get(resourceEndpoint+param, function(request, response){
+    const { id } = request.params
+    const client = data.find(foundClient => foundClient.id == id )
+
+    if(!client) return response.status(204).json()
+    
+    response.json(client)
+})
+
 
 
 server.listen(3000, function(){
