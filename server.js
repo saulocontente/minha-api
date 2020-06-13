f5b30af8dd31b7d057e238024c1bade733e53509
Saulo Contente <scontente@aliancahospitalar.com>
2020-06-12 21:46:57 -0300

    Implements methods PUT and POST

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ server.get(resourceEndpoint+param, function(request, response){
     response.json(client)
 })
 
+server.post(resourceEndpoint, function(request, response){
+    const {  id  ,  name  } = request.body
+
+    //Salvar
+
+    response.status(201).json({  id  ,  name  })
+})
+
+server.put(resourceEndpoint+param, function(request, response){
+const { id } = request.params
+const client = data.find(foundClient => foundClient.id == id)
+
+if(!client) return response.status(204).json()
+
+const {  name  } = request.body
+
+response.json({  id  ,  name  })
+
+response.json(client)
+
+})
+server.delete(resourceEndpoint, function(request, response){})
 
 
 server.listen(3000, function(){
