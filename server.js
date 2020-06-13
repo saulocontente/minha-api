412f5206b2aa6fca32181537ea84bfffcbe9b0b1
Saulo Contente <scontente@aliancahospitalar.com>
2020-06-12 21:45:08 -0300

    Implement the method to Start the server

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express')
-const app = express()
+const server = express()
 const data = require('./data.json')
-
-app.use(express.json())
-
-app.get('/client', function(request, response){
-    response.json(data)
-})
-app.post('/client', function(request, response){})
-app.put('/client', function(request, response){})
-app.delete('/client', function(request, response){})
+const resourceEndpoint = '/client'
+const param = '/:id'
 
 
-app.listen(3000, function(){
+
+server.listen(3000, function(){
     console.log('Server is running')
 })
